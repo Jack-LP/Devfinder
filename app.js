5d6878e3f1b55cfcc24f1c5e552ab3da2a92f943
Jack Paget <jackpaget1@gmail.com>
2022-04-08 12:22:53 +0100

    feat Mobile UI & API call

--- app.js
+++ app.js
@@ -1,11 +1,89 @@
+// search
+const searchSection = document.getElementById('search-section');
+const userInput = document.getElementById('username-input');
+const searchBtn = document.getElementById('search-btn');
+
+// user display
+const userAvatar = document.getElementById('user-avatar');
+const userName = document.getElementById('user-name');
+const userHandle = document.getElementById('user-handle');
+const userDate = document.getElementById('user-date');
+const userBio = document.getElementById('user-bio');
+
+// user stats
+const userRepos = document.getElementById('user-repos');
+const userFollowers = document.getElementById('user-followers');
+const userFollowing = document.getElementById('user-following');
+
+// user links
+const userLinks = document.querySelectorAll('.user-link');
+
+const userLocation = document.getElementById('user-location');
+const userWebsite = document.getElementById('user-website');
+const userTwitter = document.getElementById('user-twitter');
+const userCompany = document.getElementById('user-company');
+
+let username = '';
+
+userInput.addEventListener('keyup', function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    searchBtn.click();
+  }
+});
+
+searchBtn.addEventListener('click', () => {
+  username = userInput.value;
+  getUserData(username);
+});
+
 async function getUserData(username) {
   const response = await fetch(`https://api.github.com/users/${username}`);
-  const userData = await response.json();
-
-  console.log(userData);
-
-  document.getElementById('user-name').textContent = userData.name;
-  document.getElementById('user-handle').textContent = `@${userData.login}`;
+  if (response.status != 200) {
+    invalidInput();
+  } else {
+    const userData = await response.json();
+    fillCard(userData);
+  }
 }
 
-getUserData(`jack-lp`);
+function fillCard(userData) {
+  searchSection.classList.remove('invalid');
+  let joinDate = userData.created_at.substr(0, 9);
+  userAvatar.src = userData.avatar_url;
+
+  if (!userData.name) {
+    userName.textContent = userData.login;
+  } else {
+    userName.textContent = userData.name;
+  }
+
+  userHandle.textContent = `@${userData.login}`;
+  userDate.textContent = `Joined ${joinDate}`;
+
+  userBio.textContent = userData.bio;
+
+  userRepos.textContent = userData.public_repos;
+  userFollowers.textContent = userData.followers;
+  userFollowing.textContent = userData.following;
+
+  userLinks.forEach((link) => {
+    let linkData = link.dataset.id;
+    if (userData[linkData] != null && userData[linkData] != '') {
+      link.classList.remove('not-availible');
+      link.textContent = userData[linkData];
+    } else {
+      link.textContent = 'Not Availible';
+      link.classList.add('not-availible');
+    }
+  });
+
+  // userLocation.textContent = userData.location;
+  // userWebsite.textContent = userData.blog;
+  // userTwitter.textContent = userData.twitter_username;
+  // userCompany.textContent = userData.company;
+}
+
+function invalidInput() {
+  searchSection.classList.add('invalid');
+}
